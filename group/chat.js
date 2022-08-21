1cf8b3a8426fc746bc85d4fe9f8648db7e34cc25
Lalit Rajak <lalit92rajak@gmail.com>
2022-08-22 00:36:56 +0530

    changes

---
+++ group/chat.js
@@ -0,0 +1,12 @@
+const express=require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+const loginRoute=require('./login')
+const messageRoute = require('./message')
+
+app.use(bodyParser.urlencoded({ extended:false}))
+
+app.use(loginRoute)
+app.use(messageRoute)
+app.listen(3000)
